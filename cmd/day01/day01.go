142256356a377e51b43b5319121eb5f5c98700e9
Jared Parsons <jaredpparsons@gmail.com>
2022-12-03 08:44:05 -0800

    day01 part 2

--- cmd/day01/day01.go
+++ cmd/day01/day01.go
@@ -3,21 +3,25 @@ package main
 import (
 	"embed"
 	"fmt"
+	"flag"
 	"github.com/jaredpar/advent2022/util"
 )
 
 //go:embed *.txt
 var f embed.FS
 
-func part1() {
-	max := 0
+func getSumOfMax(count int) {
+	maxes := make([]int, count)
 	current := 0
 	lines := util.MustReadLines(f, "input1.txt")
 	for _, line := range lines {
 		if util.IsWhitespace(line) {
 			fmt.Printf("Current calories %d\n", current)
-			if current > max {
-				max = current
+			for i, max := range maxes {
+				if current > max {
+					maxes[i] = current
+					break
+				}
 			}
 
 			current = 0
@@ -28,10 +32,29 @@ func part1() {
 		current = current + value
 	}
 
-	fmt.Printf("Max calories is %d\n", max)
+	total := 0
+	for _, max := range maxes {
+		fmt.Printf("Individual max is %d\n", max)
+		total += max
+	}
+
+	fmt.Printf("Max calories is %d\n", total)
+}
+
+func part1() {
+	getSumOfMax(1)
+}
+
+func part2() {
+	getSumOfMax(3)
 }
 
 func main() {
-	part1()
-
+	p1 := flag.Bool("part1", false, "run part1")
+	flag.Parse()
+	if *p1 {
+		part1()
+	} else {
+		part2()
+	}
 }
